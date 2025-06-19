0402bb1579e3d5ef73941c11cac8a4281673ca08
Asesh Nemal <aseshnemal@gmail.com>
2025-06-19 18:25:06 +0530

    Revert "Changes 1"
    This reverts commit 7a24a85c110a362a69a313a642332196e54ba40a.

--- Pet Pro IOS/ContentView.swift
+++ Pet Pro IOS/ContentView.swift
@@ -9,46 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                Spacer(minLength: 150)
-                Image("image_4_removebg_preview")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 372, height: 424)
-                    .padding(.horizontal, 16)
-                Spacer(minLength: 20)
-                Text("Welcome to")
-                    .font(.system(size: 34, weight: .bold))
-                Text("PetPro")
-                    .font(.system(size: 34, weight: .bold))
-                Spacer()
-                HStack {
-                    NavigationLink(destination: LoginView()) {
-                        Text("Skip")
-                            .font(.system(size: 17))
-                            .padding(.horizontal, 20)
-                            .padding(.vertical, 10)
-                            .foregroundColor(.blue)
-                            .background(Color.clear)
-                            .cornerRadius(8)
-                    }
-                    Spacer()
-                    NavigationLink(destination: RegistrationView()) {
-                        Text("Get Start")
-                            .font(.system(size: 17))
-                            .padding(.horizontal, 20)
-                            .padding(.vertical, 10)
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .cornerRadius(8)
-                    }
-                }
-                .padding(.horizontal, 40)
-                .padding(.bottom, 40)
-            }
-            .navigationBarHidden(true)
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
